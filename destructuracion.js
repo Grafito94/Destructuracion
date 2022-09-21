534acd97da2a5a6c2a3b642645ce9e3aa1eed73f
Santiago Gutierrez Cardona <104087575+Grafito94@users.noreply.github.com>
2022-09-21 06:42:39 -0500

    first commit

---
+++ destructuracion.js
@@ -0,0 +1,58 @@
+//Problema 1 
+
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar) //Tesla
+console.log(otherRandomCar) //Mercedes
+
+
+//Problema 2 //
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+//console.log(name); //Error porque no estaba definido
+console.log(otherName); //Elon
+
+
+//Problema 3//
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password); //12345
+console.log(hashedPassword); //Undefined ya que no se le ha asignado un valor dentro del dobjeto person
+
+
+//Problema 4//
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second); //2 == 5 --- False
+console.log(first == third); //2 == 2 --- True
+
+
+//Problema 5//
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key); //Value
+console.log(secondKey); //[1, 5, 1, 8, 3, 3]
+console.log(secondKey[0]); //1
+console.log(willThisWork); //5
